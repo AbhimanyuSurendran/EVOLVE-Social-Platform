823b0a91955d8baa86e538f957fadea4da0c9eeb
Abhimanyu Surendran <abhiyoyo165@gmail.com>
2025-12-26 20:57:10 +0530

    trying

--- backend/server.cjs
+++ backend/server.cjs
@@ -1979,6 +1979,6 @@ app.listen(PORT, () =>
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
